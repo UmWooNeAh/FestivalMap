910dad49f302516a2b2a02f69ede10d0c688f0bf
gunwoo99 <miniapple212@gmail.com>
2023-04-27 20:26:32 +0900

    FIx : Mypage
    listview안나오던거 클래스로 만들어서 column으로 구현

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,9 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     // primary color declaration
-    final Color firstColor  = Colors.black;
+    final Color firstColor = Colors.black;
     final Color secondColor = Colors.grey;
-    final Color thirdColor  = Colors.white;
+    final Color thirdColor = Colors.white;
     final ButtonStyle myPageMenuButton = ElevatedButton.styleFrom(
         backgroundColor: Colors.white,
         shape: const RoundedRectangleBorder(
@@ -39,12 +39,14 @@ class _MyPageState extends State<MyPage> {
         )
     ); // myPageMenuButton style declaration
     final ButtonStyle myPageMenuButton_2 = ElevatedButton.styleFrom(
-      backgroundColor: secondColor,
-      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 70),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(1)),
-      )
+        backgroundColor: secondColor,
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 70),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(1)),
+        )
     ); // myPageMenuButton_2 style declaration
+
+
 
     Divider menuDivider = Divider(
       thickness: 1,
@@ -52,7 +54,13 @@ class _MyPageState extends State<MyPage> {
       endIndent: 20,
       color: firstColor,
     );
-    final List<String> entries   = <String>["Notice", "Event", "Support", "Terms"];
+
+    final List<String> entries = <String>[
+      "Notice",
+      "Event",
+      "Support",
+      "Terms"
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -71,86 +79,107 @@ class _MyPageState extends State<MyPage> {
         ],
       ),
       body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Text("조우석 님",),
+                    Text(
+                      "Platinum Level", style: TextStyle(color: Colors.pink,),),
+                  ],
+                ),
+              ],
+            ),
+            menuDivider,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  child: Icon(Icons.chat_bubble, color: secondColor,),
+                  onPressed: () {},
+                  style: myPageMenuButton,
+                ),
+                ElevatedButton(
+                  child: Icon(Icons.card_membership, color: secondColor,),
+                  onPressed: () {},
+                  style: myPageMenuButton,
+                ),
+                ElevatedButton(
+                  child: Icon(Icons.cake, color: secondColor,),
+                  onPressed: () {},
+                  style: myPageMenuButton,
+                ),
+                ElevatedButton(
+                  child: Icon(Icons.list, color: secondColor,),
+                  onPressed: () {},
+                  style: myPageMenuButton,
+                ),
+              ],
+            ),
+            Divider(thickness: 0, height: 20, color: thirdColor,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  child: Text("쿠폰함", style: TextStyle(color: Colors.black,),),
+                  onPressed: () {},
+                  style: myPageMenuButton_2,
+                ),
+                TextButton(
+                  child: Text("멤버쉽", style: TextStyle(color: Colors.black),),
+                  onPressed: () {},
+                  style: myPageMenuButton_2,
+                ),
+              ],
+            ),
+            Divider(thickness: 0, height: 20, color: thirdColor,),
+            Column(
+              children: [
+
+                menuList(str: entries[0], color: secondColor),
+                menuDivider,
+                menuList(str: entries[1], color: secondColor),
+                menuDivider,
+                menuList(str: entries[2], color: secondColor),
+                menuDivider,
+                menuList(str: entries[3], color: secondColor),
+                menuDivider,
+              ],
+            )
+          ]
+      ),
+    );
+  }
+}
+
+class menuList extends StatelessWidget {
+  final String str;
+  final Color color;
+  const menuList({Key? key, required this.str, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Column(
-                children: [
-                  Text("조우석 님",),
-                  Text("Platinum Level", style: TextStyle(color: Colors.pink,),),
-                ],
-              ),
-            ],
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(str, style: TextStyle(fontSize: 15),),
           ),
-          menuDivider,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(
-                child: Icon(Icons.chat_bubble, color: secondColor,),
-                onPressed: (){},
-                style: myPageMenuButton,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: (){},
+              child: Icon(Icons.arrow_forward_ios, color: color,),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
               ),
-              ElevatedButton(
-                child: Icon(Icons.card_membership, color: secondColor,),
-                onPressed: (){},
-                style: myPageMenuButton,
-              ),
-              ElevatedButton(
-                child: Icon(Icons.cake, color: secondColor,),
-                onPressed: (){},
-                style: myPageMenuButton,
-              ),
-              ElevatedButton(
-                child: Icon(Icons.list, color: secondColor,),
-                onPressed: (){},
-                style: myPageMenuButton,
-              ),
-            ],
+            ),
           ),
-          Divider(thickness: 0, height: 20, color: thirdColor,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextButton(
-                child: Text("쿠폰함", style: TextStyle(color: Colors.black,),),
-                onPressed: (){},
-                style: myPageMenuButton_2,
-              ),
-              TextButton(
-                child: Text("멤버쉽", style: TextStyle(color: Colors.black),),
-                onPressed: (){},
-                style: myPageMenuButton_2,
-              ),
-            ],
-          ),
-
-          ListView.builder(
-            itemCount: 4,
-            itemBuilder: (BuildContext context, int index){
-              return Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(entries[index], style: TextStyle(fontSize: 15),),
-                      ),
-                      ElevatedButton(
-                          onPressed: (){},
-                          child: Icon(Icons.arrow_back_ios, color: secondColor,)),
-                    ],
-                  ),
-                  menuDivider,
-                ],
-              );
-
-            },
-          ),
-
-        ]
+        ],
       ),
     );
   }
