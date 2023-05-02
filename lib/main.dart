35c51edeb19710a357893781ac64c9258221eb11
gunwoo99 <miniapple212@gmail.com>
2023-05-03 02:06:45 +0900

    Feat : HomePage
    홈화면에서 쓸 image asset 폴더생성
    image 2장 업로드
    홈화면 네비게이션바 추가
    홈화면 배너 번호 추가
    홈화면 핫한 페스티벌 객체 만드는중

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:festivalmap/FestivalDetailPage.dart';
+import 'package:festivalmap/mypage.dart';
+import 'package:floating_bottom_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,6 +23,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NaverMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(35.8, 128.5),
+          zoom: 12,
+        ),
+      ),
+
+    );
+  }
+}
+
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -27,49 +54,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool circleButtonToggle = false;
+  int index = 2;
+
   // primary color declaration
-  final Color firstColor = Colors.black;
+  final Color firstColor = Colors.pinkAccent;
   final Color secondColor = Colors.grey;
   final Color thirdColor = Colors.white;
   List<String> eventImage = <String>["a", "b", "c"];
 
   final List<String> images = [
-    'https://via.placeholder.com/500x200?text=Banner%201',
-    'https://via.placeholder.com/500x200?text=Banner%202',
-    'https://via.placeholder.com/500x200?text=Banner%203',
+      'assets/img.png',
+      'assets/img.png',
+      'assets/img.png',
   ];
+  int value = 1;
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 20,
         backgroundColor: thirdColor,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: firstColor,),
-          onPressed: () {},
-        ),
-        title: Text("My Page", style: TextStyle(color: firstColor,),),
+        title: Text("축제지도", style: TextStyle(color: firstColor,),),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.settings, color: Colors.black,),
+            icon: Icon(Icons.search_sharp, color: Colors.black,),
             onPressed: () {},
           ),
         ],
       ),
 
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-            child: CarouselSlider(
-              options: CarouselOptions(),
-              items: images.map(
-                  (item) => Container(
-                    child: Image.network(item, fit: BoxFit.cover, width: 500),
-                  )
-              ).toList(),
-            ),
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              Container(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    onPageChanged: (index, reason){
+                      setState(() {
+                        value = index + 1;
+                      });
+                    }),
+                  items: images.map(
+                          (item) => Container(
+                        child: Image.asset(item, fit: BoxFit.fitHeight),
+                      )
+                  ).toList(),
+                ),
+              ),
+
+              Container(
+                child: Text("<$value/12>", style: TextStyle(color: Colors.white),),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
           ),
+
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -90,22 +137,150 @@ class _HomePageState extends State<HomePage> {
                   style: ElevatedButton.styleFrom(
                     padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     )
                 ),
               ),
               HomePageCategoricalButton(buttonIcon: Icon(Icons.add)),
             ],
           ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child:Text("#요즘 핫한 페스티벌", style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Image.asset("assets/img_1.png", fit: BoxFit.fitHeight,),
+                  ),
+
+
+                  Expanded(
+                    child : Padding(
+                      padding: EdgeInsets.fromLTRB(0,0,20,0),
+                      child:Container(
+                        height: 150,
+                        decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: [
+                            Text("2023 Daegu Hiphop Festival"),
+                            Text("asdf"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(),
+            ],
+          )
         ],
       ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.map), label: "map"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        barColor: Colors.white,
+        controller: FloatingBottomBarController(initialIndex: 1),
+
+        bottomBar: [
+          BottomBarItem(
+            icon: const Icon(Icons.category_outlined, size: 30),
+            iconSelected: const Icon(Icons.category_outlined,
+                color: Colors.red,
+                size: 30),
+            title: "카테고리",
+            dotColor: Colors.red,
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+          ),
+          BottomBarItem(
+            icon:
+            const Icon(Icons.pin_drop_outlined, size: 30),
+            iconSelected: const Icon(Icons.pin_drop_outlined,
+                color: Colors.red,
+                size: 30),
+            title: "지도",
+            dotColor: Colors.red,
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+          ),
+          BottomBarItem(
+            icon: const Icon(Icons.bookmark_border, size: 30),
+            iconSelected: const Icon(Icons.bookmark_border,
+                color: Colors.red,
+                size: 30),
+            title: "북마크",
+            dotColor: Colors.red,
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+          ),
+          BottomBarItem(
+            icon:
+            const Icon(Icons.tag_faces, size: 30),
+            iconSelected: const Icon(Icons.tag_faces,
+                color: Colors.red,
+                size: 30),
+            title: "마이페이지",
+            dotColor: Colors.red,
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+          ),
         ],
+        bottomBarCenterModel: BottomBarCenterModel(
+          centerBackgroundColor: Colors.red,
+          centerIcon: const FloatingCenterButton(
+            child: Icon(
+              Icons.add,
+              color: AppColors.white,
+            ),
+          ),
+          centerIconChild: [
+            FloatingCenterButtonChild(
+              child: const Icon(
+                Icons.home,
+                color: AppColors.white,
+              ),
+            ),
+            FloatingCenterButtonChild(
+              child: const Icon(
+                Icons.access_alarm,
+                color: AppColors.white,
+              ),
+            ),
+            FloatingCenterButtonChild(
+              child: const Icon(
+                Icons.ac_unit_outlined,
+                color: AppColors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
